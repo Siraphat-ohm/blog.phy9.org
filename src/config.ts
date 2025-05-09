d9334f1f1c69d2d3f30a7d02405cd9de52c9cb99
Siraphat-Ohm <siraphat975@gmail.com>
2025-05-10 02:10:08 +0700

    nav bar

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "Siraphat",
-  subtitle: "",
+  title: "siph",
+  subtitle: "blog",
   lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -41,7 +41,7 @@ export const siteConfig: SiteConfig = {
 export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
-    LinkPreset.Archive,
+    // LinkPreset.Archive,
     LinkPreset.About,
     {
       name: "GitHub",
